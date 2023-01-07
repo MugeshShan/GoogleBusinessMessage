c795a82500d8bc7e213108e01d915827225b99a7
Mugesh Shanmugavel <mugesh.shanmugavel@justanswer.com>
2023-01-07 11:43:21 +0530

    Initial Commit

---
+++ BusinessMessageApp/Controllers/BusinessMessageController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessMessageApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace BusinessMessageApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusinessMessageController : ControllerBase
+    {
+        public BusinessMessageController()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> Callback([FromBody] dynamic request)
+        {
+            var dict = JsonSerializer.Deserialize<Dictionary<string, string>>(request);
+            var messageModel = new MessageModel();
+            if (dict != null && dict.ContainsKey("clientToken"))
+            {
+                messageModel.ClientToken = dict["clientToken"];
+            }
+            if (dict != null && dict.ContainsKey("secret"))
+            {
+                messageModel.Secret = dict["secret"];
+            }
+
+            return Ok("secret:" + messageModel.Secret);
+        }
+    }
+}
---
+++ BusinessMessageApp/Models/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BusinessMessageApp.Models
+{
+    public class MessageModel
+    {
+
+        [JsonPropertyName("clientToken")]
+        public string ClientToken { get; set; }
+
+        [JsonPropertyName("secret")]
+        public string Secret { get; set; }
+    }
+}
